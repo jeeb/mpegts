9012a30d0b0a213ced7f15dd35b8ddfadd66a6aa
Jan E <jeebjp@gmail.com>
2012-02-15 19:19:01 +0200

    Remove herpy derpy whitespace. Code style still sucks.

--- parser.c
+++ parser.c
@@ -28,16 +28,16 @@ int main( int argc, char** argv ) {
     /* For parsing Transport Stream Packets */
     uint16_t up_to_pid; /* First 16 bits after sync_byte until PID. */
     uint8_t after_pid; /* 8 bits after PID. */
-    
+
     /* Load the file and check its size. */
     file = fopen64( "herp.ts", "rb" );
-    
+
     fseeko64( file, 0L, SEEK_END );
     fsize = ftello64( file );
     fseeko64( file, 0L, SEEK_SET );
-    
+
     transport_packet_header tsheader;
-    
+
     do
     {
         derp = fread( &read_data, sizeof( uint8_t ), 1, file );
@@ -50,18 +50,17 @@ int main( int argc, char** argv ) {
             if( counter < 4 ) {
                 last_sync_byte_pos[counter] = (uint64_t)(ftello64(file) -1);
                 counter += 1;
-            } 
-            
+            }
+
             printf("Sync byte found at position: 0x%lX\n", (uint64_t)ftello64(file) -1);
-            if( counter > 1 && counter )
-            
+
             derp = fread( &up_to_pid, sizeof( uint16_t ), 1, file );
             if( derp!= 1 )
             {
                 printf( "Derped at reading the first 16 bits after sync byte.\n" );
                 return 1;
             }
-            
+
             tsheader.transport_error_indicator = (uint8_t)(up_to_pid >> 15);
             tsheader.payload_unit_start_indicator = (uint8_t)(up_to_pid << 1) >> 15;
             tsheader.transport_priority = (uint8_t)(up_to_pid << 2) >> 15;
@@ -69,7 +68,7 @@ int main( int argc, char** argv ) {
             printf("TEI: %u, PUSI: %u, TP: %u\n", tsheader.transport_error_indicator,
                     tsheader.payload_unit_start_indicator, tsheader.transport_priority );
             printf("Seeming PID: 0x%X\n\n", tsheader.pid);
-            
+
             derp = fread( &after_pid, sizeof( uint8_t ), 1, file );
             if( derp!= 1 )
             {
@@ -85,15 +84,15 @@ int main( int argc, char** argv ) {
                 printf("\n");
                 counter = 5;
             }
-            
+
             tsheader.transport_scrambling_control = after_pid >> 6;
             tsheader.adaptation_field_control = (after_pid << 2) >> 6;
             tsheader.continuity_counter = (after_pid << 4 ) >> 4;
-            
+
         } else
             continue;
     } while( derp == 1 );
-    
+
     return ( EXIT_SUCCESS );
 }
 
